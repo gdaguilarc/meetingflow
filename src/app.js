7517b2c6141070fbef8d9d47f3d2eaeee56baead
David Aguilar <gd.aguilarc@gmail.com>
2019-06-08 11:17:47 -0500

    Flash login massage added to local

--- src/app.js
+++ src/app.js
@@ -83,6 +83,7 @@ app.use(passport.session());
 
 app.use((req, res, next) => {
   app.locals.signUpMessage = req.flash('signupMessage');
+  app.locals.signInMessage = req.flash('signinMessage');
   app.locals.user = req.user;
   next();
 });
