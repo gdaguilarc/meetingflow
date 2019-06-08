411ce87b966882fe78efe59e32865ecdc4a8bd9a
David Aguilar <gd.aguilarc@gmail.com>
2019-06-08 14:24:42 -0500

    Creating first time Schema

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose';
+import { setup } from './setup-db';
 
 mongoose
   .connect(process.env.DB_HOST, { useNewUrlParser: true })
   .then(db => {
     console.log('Successfully connected to the database ...');
     console.log('Waiting for operations ...');
+    setup();
   })
   .catch(err => {
     console.log(err);
