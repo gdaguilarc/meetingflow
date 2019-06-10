864a89197c0ec5aac5a8a4a19b00a30835174e7d
David Aguilar <gd.aguilarc@gmail.com>
2019-06-10 07:34:23 -0500

    Guest new guest route

--- src/routes/guest.js
+++ src/routes/guest.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { newGuest } from '../controllers/guest-controller';
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
@@ -6,5 +7,9 @@ const router = express.Router();
 router.get('/', (req, res) =>
   res.render('guests', { layout: 'default', template: 'guess-template' })
 );
+
+router.post('/register-guest', newGuest, (req, res) => {
+  res.redirect('/signin');
+});
 
 export default router;
