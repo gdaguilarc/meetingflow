8d7b92a92090f92daa5f1f29315af0099e50fb3d
Rodrigo Cadena <a01652141@itesm.mx>
2019-06-11 23:41:37 -0500

    Comment changes

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,3 +1,5 @@
 // function isSetup() {
 //   // TODO: create method
+// query para setup dentro de colección aplication (firstTimeSetup) checar si es falso
+// hacerlo en aplication-controller
 // }
