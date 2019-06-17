89b736e0802868dd0291f62ba7cb4c1b2b41ad02
David Aguilar <gd.aguilarc@gmail.com>
2019-06-17 07:54:17 -0500

    WIP: Send every registration a mail

--- src/controllers/guest-controller.js
+++ src/controllers/guest-controller.js
@@ -2,14 +2,17 @@ import Guest from '../models/Guest-model';
 import { isEmail } from 'validator';
 import phone from 'phone';
 import User from '../models/User-model';
+import { welcomeGuest } from '../misc/mails/mail-templates';
+import { sendMail } from './mail-sender';
+import { getFirstName } from '../misc/tools/strings';
 
 /**
  * Writes in the database the new guest
  * @param {*} req
  * @param {*} res
+ * @param {*} next
  */
-async function newGuest(req, res) {
-  console.log(req.body);
+async function newGuest(req, res, next) {
   if (isValidData(req)) {
     const guest = new Guest();
 
@@ -18,13 +21,25 @@ async function newGuest(req, res) {
     guest.organization = req.body.organization;
     guest.phone = formatPhone(req.body.phone);
 
-    const user = await User.findOne({ name: req.body.host });
+    const { organizationName } = await User.findOne({});
 
-    // TODO: Handle the not host
-    guest.host = user.id;
-    await guest.save();
+    // TODO: Get the id not the name
+    guest.id = hostDesignator(req.body.host);
+
+    await guest.save(req.body.host);
+
+    sendMail(
+      req.body.email,
+      `Welcome ${organizationName}!`,
+      'Welcome',
+      // TODO: Assignte a room
+      welcomeGuest(getFirstName(req.body.name), organizationName, guest.host, 'Maya')
+    );
+
+    next();
 
     // TODO: Send mail to the host
+    // TODO: if no host, send mail to designated emial
   } else {
     req.flash('guestRegisterMessage', 'Please enter all the data requested');
     res.redirect('/');
@@ -58,5 +73,11 @@ function isValidData(req) {
 function formatPhone(number, regionCode = 'MX') {
   return phone(number, regionCode)[0];
 }
+
+/**
+ *  Designates the host of the guest
+ * @param {String} host
+ */
+function hostDesignator(host) {}
 
 export { newGuest };
