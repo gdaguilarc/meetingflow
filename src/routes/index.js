bc1d4d87e7fb53de35e9ac3ff2193309df03c7ed
Rodrigo Cadena <a01652141@itesm.mx>
2019-06-29 18:23:05 -0500

    update index for onboarding

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,7 +10,7 @@ import express from 'express';
 import guestRoutes from './guest';
 import userRoutes from './user';
 import adminRoutes from './admin';
-import firstTimeRoutes from './first-time';
+import onBoardingRoutes from './OnBoarding/onBoarding';
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
@@ -18,6 +18,6 @@ const router = express.Router();
 router.use('/', guestRoutes);
 router.use('/', userRoutes);
 router.use('/admin', adminRoutes);
-router.use('/setup', firstTimeRoutes);
+router.use('/setup', onBoardingRoutes);
 
 export default router;
