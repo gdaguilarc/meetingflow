b147034377ec0848a52647ba43287a2096c62108
Rodrigo Cadena <a01652141@itesm.mx>
2019-06-29 18:22:34 -0500

    guest get

--- src/routes/guest.js
+++ src/routes/guest.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import { newGuest } from '../controllers/guest-controller';
+import '../controllers/application-controller';
+import { isSetup } from '../controllers/application-controller';
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-router.get('/', (req, res, next) =>
-  res.render('guests', { layout: 'default', template: 'guess-template' })
-);
+router.get('/', async (req, res, next) => {
+  if (!isSetup()) {
+    res.redirect('/setup/introduction-to-meetings');
+  } else {
+    res.render('guests', { layout: 'default', template: 'guess-template' });
+  }
+});
 
 router.post('/register-guest', newGuest, (req, res, next) => {
   res.redirect('/post');
