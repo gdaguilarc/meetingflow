ae44eeed4ae11eec7f24ca708985533302b97284
Rodrigo Cadena <a01652141@itesm.mx>
2019-07-01 11:40:41 -0500

    Redirection removed

--- src/routes/guest.js
+++ src/routes/guest.js
@@ -1,17 +1,12 @@
 import express from 'express';
 import { newGuest } from '../controllers/guest-controller';
 import '../controllers/application-controller';
-import { isSetup } from '../controllers/application-controller';
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-  if (!isSetup()) {
-    res.redirect('/setup/introduction-to-meetings');
-  } else {
-    res.render('guests', { layout: 'default', template: 'guess-template' });
-  }
+  res.render('guests', { layout: 'default', template: 'guess-template' });
 });
 
 router.post('/register-guest', newGuest, (req, res, next) => {
