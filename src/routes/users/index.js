1d2cba9b042a37ff98b5049f88f8c8736cc4297d
gdaguilarc <gd.aguilarc@gmail.com>
2019-07-11 18:26:52 -0500

    Adding some routes to the users

--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -1,3 +1,11 @@
-import routes from './auth.service';
+import express from 'express';
+import routesAuth from './auth.service';
+import routesUsers from './user';
 
-export default routes;
+// eslint-disable-next-line new-cap
+const router = express.Router();
+
+router.use('/', routesAuth);
+router.use('/', routesUsers);
+
+export default router;
