60b2dc317640cb4b84e6fe236afc78517fe0135e
Rodrigo Cadena <a01652141@itesm.mx>
2019-07-29 16:43:18 -0500

    Mail templates

---
+++ src/misc/mails/footer.js
@@ -0,0 +1,50 @@
+const footer = companyName => `</div>
+</div>
+<!--[if mso]></td></tr></table><![endif]-->
+<!--[if (!mso)&(!IE)]><!-->
+</div>
+<!--<![endif]-->
+</div>
+</div>
+<!--[if (mso)|(IE)]></td></tr></table><![endif]-->
+<!--[if (mso)|(IE)]></td></tr></table></td></tr></table><![endif]-->
+</div>
+</div>
+</div>
+<div style="background-color:#B9B9B9;">
+<div class="block-grid" style="Margin: 0 auto; min-width: 320px; max-width: 600px; overflow-wrap: break-word; word-wrap: break-word; word-break: break-word; background-color: #B9B9B9;">
+<div style="border-collapse: collapse;display: table;width: 100%;background-color:#B9B9B9;">
+<!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0" style="background-color:#B9B9B9;"><tr><td align="center"><table cellpadding="0" cellspacing="0" border="0" style="width:600px"><tr class="layout-full-width" style="background-color:#B9B9B9"><![endif]-->
+<!--[if (mso)|(IE)]><td align="center" width="600" style="background-color:#B9B9B9;width:600px; border-top: 0px solid transparent; border-left: 0px solid transparent; border-bottom: 0px solid transparent; border-right: 0px solid transparent;" valign="top"><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding-right: 0px; padding-left: 0px; padding-top:0px; padding-bottom:0px;"><![endif]-->
+<div class="col num12" style="min-width: 320px; max-width: 600px; display: table-cell; vertical-align: top; width: 600px;">
+<div style="width:100% !important;">
+<!--[if (!mso)&(!IE)]><!-->
+<div style="border-top:0px solid transparent; border-left:0px solid transparent; border-bottom:0px solid transparent; border-right:0px solid transparent; padding-top:0px; padding-bottom:0px; padding-right: 0px; padding-left: 0px;">
+<!--<![endif]-->
+<!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding-right: 10px; padding-left: 10px; padding-top: 15px; padding-bottom: 10px; font-family: Tahoma, Verdana, sans-serif"><![endif]-->
+<div style="color:#FFFFFF;font-family:'Lato', Tahoma, Verdana, Segoe, sans-serif;line-height:150%;padding-top:15px;padding-right:10px;padding-bottom:10px;padding-left:10px;">
+<div style="font-size: 12px; line-height: 18px; font-family: 'Lato', Tahoma, Verdana, Segoe, sans-serif; color: #FFFFFF;">
+<p style="font-size: 14px; line-height: 21px; text-align: center; margin: 0;">Copyright © ${companyName} 2019<a href="http://www.itd.com.mx/aviso-privacidad.php" rel="noopener" style="color: #FFFFFF;" target="_blank">        Aviso de Privacidad</a></p>
+</div>
+</div>
+<!--[if mso]></td></tr></table><![endif]-->
+<!--[if (!mso)&(!IE)]><!-->
+</div>
+<!--<![endif]-->
+</div>
+</div>
+<!--[if (mso)|(IE)]></td></tr></table><![endif]-->
+<!--[if (mso)|(IE)]></td></tr></table></td></tr></table><![endif]-->
+</div>
+</div>
+</div>
+<!--[if (mso)|(IE)]></td></tr></table><![endif]-->
+</td>
+</tr>
+</tbody>
+</table>
+<!--[if (IE)]></div><![endif]-->
+</body>
+</html>`;
+
+export { footer };
--- src/misc/mails/mail-templates.js
+++ src/misc/mails/mail-templates.js
@@ -1,21 +1,24 @@
+import { header } from './header';
+import { footer } from './footer';
+
 /**
  *  This is the template of the email for the new users
  * @param {String} user
+ * @param {String} companyName
  * @return {String} template
  */
-function welcomeUser(user) {
+function welcomeUser(user, companyName) {
   return `
-      <div>
-        <h1>Bienvenido ${user}!!</h1>
+  ${header(companyName)}
+  <p style="font-size: 12px; line-height: 33px; text-align: center; margin: 0;"><span style="font-size: 28px;">Hola ${user}!!</span></p>
         <h3>Bienvenido al sistema "Meetingflow"</h3>
         <h3>Ahora podras disfrutar de los siguientes beneficios:</h3>
-
         <ul>
           <li>Aparata salas para tus juntas</li>
           <li>Registra a tus invitados</li>
           <li>Envia invitaciones</li>
         </ul>
-      </div>
+  ${footer(companyName)}
       `;
 }
 
@@ -29,13 +32,33 @@ function welcomeUser(user) {
  */
 function welcomeGuest(guest, company, host, room) {
   return `
-      <div>
-        <h1>Bienvenido ${guest}</h1>
-        <h3>Bienvenido a ${company}, estamos muy contentos por su visita!</h3>
-        <h3>${host} vendra por ti en unos momentos </h3>
-        <h3>Tu junta se llevara a cabo en "${room}"</h3>
-      </div>
+  ${header(company)}
+  <p style="font-size: 12px; line-height: 33px; text-align: center; margin: 0;"><span style="font-size: 28px;">Hola ${guest}!!</span></p>
+  <h3>Bienvenido a ${company}, estamos muy contentos por tu visita!</h3>
+  <h3>${host} vendra por ti en unos momentos </h3>
+  <h3>Tu junta se llevara a cabo en "${room}"</h3>
+  ${footer(company)}
 `;
 }
 
-export { welcomeUser, welcomeGuest };
+/**
+ *
+ * @param {String} user
+ * @param {String} room
+ * @param {String} location
+ * @param {String} date
+ * @param {String} startTime
+ * @param {String} endTime
+ * @param {String} companyName
+ * @return {String} template
+ */
+function reservationMade(user, room, location, date, startTime, endTime, companyName) {
+  return `
+  ${header(companyName)}
+  <p style="font-size: 12px; line-height: 33px; text-align: center; margin: 0;"><span style="font-size: 28px;">Hola ${user}!!</span></p>
+  <h3>Tu reservación ha sido exitosa!</h3>
+  <h3>La ${room} de ${location} ha quedado apartada el día ${date} de ${startTime} a ${endTime} hrs.</h3>
+  ${footer(companyName)}`;
+}
+
+export { welcomeUser, welcomeGuest, reservationMade };
